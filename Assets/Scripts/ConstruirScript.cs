f10a3c72841a9b0f2e27675a55aab8fa1c583db3
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-07-30 10:38:31 -0300

    NICHO CHORRO 1

---
+++ Assets/Scripts/ConstruirScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstruirScript : MonoBehaviour
+{
+
+    public GameObject torretaSeleccionada;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Instantiate(torretaSeleccionada, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    private void OnMouseEnter()
+    {
+        sr.color = Color.green;
+    }
+
+    private void OnMouseExit()
+    {
+        sr.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/TorretaScript2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TorretaScript2 : MonoBehaviour
+{
+
+    //Objetos
+
+    public Transform punta;
+    public Transform target;
+    public GameObject bala;
+    public LayerMask enemigos;
+
+    //Variables
+
+    public float rango;
+    public float rotationSpd;
+    public bool isShooting = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bala.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            FindTarget();
+            return;
+        }
+
+        RotateTowardsTarget();
+        if (!CheckTargetRange())
+        {
+            target = null;
+            isShooting = false;
+            bala.SetActive(false);
+        }
+        else
+        {
+            if (isShooting == false)
+            {
+                isShooting = true;
+                bala.SetActive(true);
+            }
+        }
+
+
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, rango, new Vector2(transform.position.x, transform.position.y), 0f, enemigos);
+
+        if (hits.Length > 0)
+        {
+            target = hits[0].transform;
+        }
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angulo = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angulo - 90));
+
+        punta.rotation = Quaternion.RotateTowards(punta.rotation, targetRotation, rotationSpd * Time.deltaTime);
+    }
+
+    private bool CheckTargetRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= rango;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, rango);
+    }
+}
