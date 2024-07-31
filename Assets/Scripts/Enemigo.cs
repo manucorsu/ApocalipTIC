9d71b8ce708a3a280021b09b55f2650c7727dccc
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-07-31 09:00:04 -0300

    eliminar a* y poner los waypoints especiales (spawners y goals)

---
+++ Assets/Scripts/Enemigo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public float speed;
+    private byte wpIndex; //por qué waypoint estás
+    public Transform[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Mover();
+    }
+
+    private void Mover()
+    {
+
+    }
+}
