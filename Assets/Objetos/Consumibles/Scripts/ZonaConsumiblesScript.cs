35efcb855fe9975c5c4a4a40609712d04b279774
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-08-13 10:40:00 -0300

    CONSTRUIR 5

--- Assets/Objetos/Consumibles/Scripts/ZonaConsumiblesScript.cs
+++ Assets/Objetos/Consumibles/Scripts/ZonaConsumiblesScript.cs
@@ -6,11 +6,15 @@ public class ZonaConsumiblesScript : MonoBehaviour
 {
 
     public GameObject consumibleSeleccionado;
+    public GameObject construir;
+    public ConstruirScriptGeneral scrConstruir;
+    public float precioSeleccionado;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        construir = GameObject.Find("Construir");
+        scrConstruir = construir.GetComponent<ConstruirScriptGeneral>();
     }
 
     // Update is called once per frame
@@ -21,8 +25,11 @@ public class ZonaConsumiblesScript : MonoBehaviour
 
     private void OnMouseDown()
     {
-        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0f;
-        Instantiate(consumibleSeleccionado, mouseWorldPos, Quaternion.identity);
+        if ((scrConstruir.plataActual - precioSeleccionado) >= 0)
+        {
+            var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseWorldPos.z = 0f;
+            Instantiate(consumibleSeleccionado, mouseWorldPos, Quaternion.identity);
+        }
     }
 }
