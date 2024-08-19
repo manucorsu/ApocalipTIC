5720a931f0850628dca0f6e588640bdaab624a10
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-08-19 15:34:43 -0300

    Commit de prueba

---
+++ Assets/Objetos/Torretas/Scripts/MejorasScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MejorasScript : MonoBehaviour
+{
+
+    //Objetos
+
+    public GameObject sceneScripts;
+    public Image cuadroMejora;
+    private EnemySpawner enemySpawner;
+
+    //Variables
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneScripts = GameObject.Find("SCENESCRIPTS");
+        enemySpawner = sceneScripts.GetComponent<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (enemySpawner.spawnear == false)
+        {
+            
+        }
+    }
+}
