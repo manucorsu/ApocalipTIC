b9d16bb63981de1b9c6fd84eeb045df1af2692f9
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-08-20 09:02:56 -0300

    MEJORAS 1

--- Assets/Objetos/Torretas/Scripts/MejorasScript.cs
+++ Assets/Objetos/Torretas/Scripts/MejorasScript.cs
@@ -9,7 +9,7 @@ public class MejorasScript : MonoBehaviour
     //Objetos
 
     public GameObject sceneScripts;
-    public Image cuadroMejora;
+    public GameObject cuadroMejora;
     private EnemySpawner enemySpawner;
 
     //Variables
@@ -20,20 +20,38 @@ public class MejorasScript : MonoBehaviour
     void Start()
     {
         sceneScripts = GameObject.Find("SCENESCRIPTS");
+        cuadroMejora = GameObject.Find("cuadroMejora");
         enemySpawner = sceneScripts.GetComponent<EnemySpawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnMouseDown()
     {
         if (enemySpawner.spawnear == false)
         {
-            
+            Vector3 posiciónDestino = new Vector3(this.transform.position.x + 3, this.transform.position.y);
+
+            if (this.gameObject.transform.position.y < -5)
+            {
+                posiciónDestino.y += 1.5f;
+            }
+
+            if (this.gameObject.transform.position.y > 4)
+            {
+                posiciónDestino.y -= 1.5f;
+            }
+
+            if (this.gameObject.transform.position.x > 7)
+            {
+                posiciónDestino.x -= 6;
+            }
+
+            cuadroMejora.transform.position = Vector3.MoveTowards(cuadroMejora.transform.position, posiciónDestino, 1000);
         }
     }
 }
