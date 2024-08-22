7dba35ad1a0f62ddbaa425a0f049090c8906e113
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-08-22 14:31:15 -0300

    ligero progreso en boss
    me siento tan solo en este bloque blendista

--- Assets/Objetos/Enemigos/Scripts/Boss.cs
+++ Assets/Objetos/Enemigos/Scripts/Boss.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class Boss : EnemigoScript
 {
+    [SerializeField] private protected SpriteRenderer sr;
+    [SerializeField] private protected Sprite[] sprites;
+    private protected byte si = 0; //sprite index
+
     private void Update()
     {
-        this.canBeEaten = false;
+        if (Input.GetKeyDown(KeyCode.B)) // Hacer boss
+        {
+            Debug.Log("Atacando jefe!");
+            this.gameObject.tag = "enemigo";
+            this.gameObject.layer = 8;
+        }
+        else if (Input.GetKeyDown(KeyCode.P)) // Hacer pacífico
+        {
+            Debug.Log("Ignorando jefe...");
+            this.gameObject.tag = "Untagged";
+            this.gameObject.layer = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) // switch sprite
+        {
+            si++;
+            if (si >= sprites.Length)
+            {
+                si = 0;
+            }
+            sr.sprite = sprites[si];
+            Destroy(this.gameObject.GetComponent<PolygonCollider2D>());
+            this.gameObject.AddComponent<PolygonCollider2D>();
+        }
     }
 }
