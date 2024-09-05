24873726d16316e3262f9459e1c0fe132ad61f5d
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-09-05 14:30:53 -0300

    boss
    + animación de spawnear enemigo
    + un montonazo de código que al final no hace nada (?) hablar con jero

--- Assets/Objetos/Enemigos/Scripts/Boss.cs
+++ Assets/Objetos/Enemigos/Scripts/Boss.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Boss : EnemigoScript
 {
-    private string activeBehaviour; //la intro no cuenta
-    private bool idle = false;
+    private bool idle;
     private bool introDone = false;
     private Dictionary<string, float> moveAnims = new Dictionary<string, float> //viva python
     {
@@ -30,18 +29,37 @@ public class Boss : EnemigoScript
     #region la intro
     private void DoIntro()
     { //recuerdos de scratch
-        SetMoveAnim("MoveLeft");
-        StartCoroutine(MoveTo(new string[] { "W1" }));
+        StartCoroutine(MoveTo(new string[] { "W1" }, new string[] { "MoveLeft" }));
     }
     private void DoIntroLaugh()
     {
         animator.SetTrigger("introLaughTrigger");
     }
     #endregion
-    private IEnumerator MoveTo(string[] wpNames, bool thenSpawnEnemies = false) // v3ify pero corrutina
+    private IEnumerator MoveTo(string[] wpNames, string[] mans, bool thenSpawnEnemies = false) // v3ify pero corrutina
     {
+        idle = false;
+        List<float> anims = new List<float>();
+        if (wpNames.Length != mans.Length)
+        {
+            Debug.LogError("MoveTo (Boss): el tamaño de wpNames y mans (move anim names) debe ser igual!!");
+        }
+        else
+        {
+            foreach (string man in mans)
+            {
+                if (moveAnims.ContainsKey(man))
+                {
+                    anims.Add(moveAnims[man]);
+                }
+                else
+                {
+                    Debug.LogError($"MoveTo: No existe la key {man} en el diccionario de moveAnims!!");
+                }
+            }
+        }
         Vector3 target = new Vector3();
-        string targetName = "";//viva este lenguaje
+        string targetName = "";
         while (false != true)
         {
             for (byte i = 0; i < wpNames.Length; i++)
@@ -51,23 +69,24 @@ public class Boss : EnemigoScript
                     if (waypoints[j].name == wpNames[i])
                     {
                         target = waypoints[j].position;
+                        while (this.transform.position != target)
+                        {
+                            SetMoveAnim(mans[i]);
+                            this.transform.position = Vector3.MoveTowards(this.transform.position, target, spd * Time.deltaTime);
+                            yield return null; //null == esperar al siguiente frame a lo Update
+                        }
                         targetName = wpNames[i];
                         break;
                     }
                 }
             }
-            while (this.transform.position != target)
-            {
-                this.transform.position = Vector3.MoveTowards(this.transform.position, target, spd * Time.deltaTime);
-                yield return null; //null == esperar al siguiente frame a lo Update
-            }
+
             if (!introDone) //en la intro se mueve a un solo wp antes de reírse, pero además hace otras cosas
             {
                 DoIntroLaugh();
                 yield return new WaitForSeconds(3f);
                 canBeShot = true;
                 introDone = true;
-                idle = true;
                 break;
             }
             else if (thenSpawnEnemies == true)
@@ -76,7 +95,7 @@ public class Boss : EnemigoScript
                 byte cuantos = (byte)Random.Range(1, 4);
                 for (byte i = 0; i > cuantos; i++)
                 {
-                    //animator.SetTrigger("spawnEnemyTrigger");
+                    animator.SetTrigger("spawnEnemyTrigger");
                     GameObject prefabElegido;
                     byte rie = (byte)Random.Range(0, pfbsEnemigos.Length);
                     prefabElegido = pfbsEnemigos[rie];
@@ -87,6 +106,7 @@ public class Boss : EnemigoScript
             }
             break;
         }
+        idle = true;
     }
     #endregion
 
@@ -117,6 +137,10 @@ public class Boss : EnemigoScript
     }
     private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.S) && idle == true)
+        {
+            Debug.Log("s");
+            MoveTo(new string[] { "W1", "W6" }, new string[] { "MoveLeft", "MoveDown" }, true);
+        }
     }
 }
