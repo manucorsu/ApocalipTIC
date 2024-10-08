ca5c101906da8063c36172aa6f8ad7ed31ce52ae
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-10-08 13:33:01 -0300

    experimento barra vida

--- Assets/Objetos/Enemigos/Scripts/HPBar.cs
+++ Assets/Objetos/Enemigos/Scripts/HPBar.cs
@@ -7,11 +7,30 @@ public class HPBar : MonoBehaviour
     [field: SerializeField] public float Max { get; private set; }
     [field: SerializeField] public float Value { get; private set; }
 
+    [SerializeField] private RectTransform _topBar;
+    [SerializeField] private RectTransform _btmBar;
+
+    private float _fullWidth;
+    private float TargetWidth => Value * _fullWidth / Max;
+    
+    private void Start()
+    {
+        _fullWidth = _topBar.rect.width;
+    }
+
     public void Change(float p)
     {
         Value = Mathf.Clamp(Value+p, 0, Max);
     }
 
+    //private IEnumerator UpdateBar(float a)
+    //{
+    //    RectTransform instantBar; // la roja
+    //    if (a >= 0) instantBar = _btmBar;
+    //    else instantBar = _topBar;
+
+    //    RectTransform smoothBar;
+    //}
     private void Update()
     {
         float a = 25;
