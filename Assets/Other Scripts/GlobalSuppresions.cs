8298b2198695e0115b27daadd5e2111e3dbd6670
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-11-09 15:21:12 -0300

    desnefastización de las barras de vida

--- Assets/Other Scripts/GlobalSuppresions.cs
+++ Assets/Other Scripts/GlobalSuppresions.cs
@@ -1,3 +1,9 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE0300:Use collection initializer", Justification = "Unity 2018")]
+#region todas? las de collection initialization (son tres sugerencias distintas no sé por qué)
+[assembly: SuppressMessage("Style", "IDE0300", Justification = "Unity 2018")]
+[assembly: SuppressMessage("Style", "IDE0305", Justification = "Unity 2018")]
+[assembly: SuppressMessage("Style", "IDE0028", Justification = "Unity 2018")]
+#endregion
+
+[assembly: SuppressMessage("Style", "IDE0044", Justification = "Breaks Unity serialization")]
